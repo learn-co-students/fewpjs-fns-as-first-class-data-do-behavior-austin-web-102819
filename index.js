ed37c71f1be093612baddc84d9f220f878aaf6e9
BrittneyHaselmayer <brittneyhaselmayer@yahoo.com>
2019-12-10 13:02:02 -0600

    Done.

--- index.js
+++ index.js
@@ -5,7 +5,20 @@ function handleClick(e) {
   displayMessage(greet(timeString))
 }
 
-/* End Given Code, don't edit above here...*/
+function greet(timeString) {
+	let time = timeString.split(':')[0];
+	if (time < 12) {
+		return 'Good Morning';
+	} else if (time >= 12 && time <= 17) {
+		return 'Good Afternoon';
+	} else if (time > 17 && time <= 24) {
+		return 'Good Evening';
+	} else {
+		return 'TEST';
+	}
+}
 
-/* Write your implementation of greet() */
-/* Write your implementation of displayMessage() */
+function displayMessage(message) {
+	const greeting = document.getElementById('greeting');
+	greeting.innerText = message;
+}
